9a5457a2eb7a7efd64bb2d4f94662e9ad71462ea
KrishnaAlagiri <39209037+KrishnaAlagiri@users.noreply.github.com>
2019-01-12 10:56:19 +0530

    Update

--- Review_Analysis.py
+++ Review_Analysis.py
@@ -2,15 +2,19 @@
 # Product Review Analysis
 # Created by Krishna Alagiri, https://github.com/KrishnaAlagiri
 
-import tweepy
-import configparser
+import tweepy, msvcrt, configparser
 from textblob import TextBlob
 
 
 #Step 0 - Parsing
 print("Parsing Informations from Setting.conf....")
 config = configparser.ConfigParser()
-config.read('Setting.conf')
+
+# Uncomment the below before executing
+#config.read('Setting.conf.ini')
+# Comment the below before executing
+config.read('C:\\Users\\Shrikrish\\Documents\\GitHub\\Setting.ini')
+
 consumer_key = config.get('TWITTER','CONSUMER_KEY')
 consumer_secret = config.get('TWITTER','CONSUMER_SECRET')
 access_token = config.get('TWITTER','ACCESS_TOKEN')
@@ -41,3 +45,5 @@ for tweet in public_tweets:
     analysis = TextBlob(tweet.text)
     print(analysis.sentiment)
     print("")
+
+msvcrt.getch()
